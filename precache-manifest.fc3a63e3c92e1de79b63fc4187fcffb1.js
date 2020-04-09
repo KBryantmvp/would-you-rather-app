d76a44c26de70a5367385b88cfabfd4feb57eb60
David's MAC <dal1986@gmail.com>
2020-04-09 13:14:40 -0500

    Updates

---
+++ precache-manifest.fc3a63e3c92e1de79b63fc4187fcffb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61daaac6a088cb0d8d8c",
+    "url": "/would-you-rather-app/static/css/main.ea619ed7.chunk.css"
+  },
+  {
+    "revision": "61daaac6a088cb0d8d8c",
+    "url": "/would-you-rather-app/static/js/main.61daaac6.chunk.js"
+  },
+  {
+    "revision": "6df345878a51ad774a2c",
+    "url": "/would-you-rather-app/static/js/1.6df34587.chunk.js"
+  },
+  {
+    "revision": "c93bd3db597d33437a8c",
+    "url": "/would-you-rather-app/static/js/runtime~main.c93bd3db.js"
+  },
+  {
+    "revision": "b1bf8156da144abf52030ee6da2068a5",
+    "url": "/would-you-rather-app/index.html"
+  }
+];
